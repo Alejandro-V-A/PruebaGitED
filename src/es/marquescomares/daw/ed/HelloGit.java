98404fc54b6b94de97925b0aacfaafbe9b63b0e6
manologomez123 <145774876+manologomez123@users.noreply.github.com>
2025-10-23 12:25:08 +0200

    Cambios añadidos por el profesor

--- src/es/marquescomares/daw/ed/HelloGit.java
+++ src/es/marquescomares/daw/ed/HelloGit.java
@@ -5,5 +5,6 @@ public class HelloGit {
 	public static void main(String[] args) {
 		System.out.println("Alejandro Valero Aguilar");
 		System.out.println("Javier Pérez Pedrosa");
+		System.out.println("Manuel Gómez Henares");
 	}
 }
