cdf7171d8a744a87916daee4e04f59142e7fac12
Alejandro Valero Aguilar <alejandrovaleroaguilar25@gmail.com>
2025-10-23 10:51:33 +0200

    First Commit Alejandro / Name Added

--- src/es/marquescomares/daw/ed/HelloGit.java
+++ src/es/marquescomares/daw/ed/HelloGit.java
@@ -3,6 +3,6 @@ package es.marquescomares.daw.ed;
 public class HelloGit {
 
 	public static void main(String[] args) {
-		System.out.println("Alejandro Valero");
+		System.out.println("Alejandro Valero Aguilar");
 	}
 }
